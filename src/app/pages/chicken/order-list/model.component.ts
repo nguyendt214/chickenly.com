30b440a45ca1b8284cf59286748fe958012ea43a
ndotrong <ndotrong@pentalog.com>
2023-11-24 12:02:36 +0700

    Order

--- src/app/pages/chicken/order-list/model.component.ts
+++ src/app/pages/chicken/order-list/model.component.ts
@@ -3,15 +3,13 @@ import { LocalDataSource } from 'ng2-smart-table';
 
 import { SmartTableData } from '../../../@core/data/smart-table';
 import { map } from 'rxjs/operators';
-import { Category, CategoryService } from '../../../main/category.service';
 import { Cart, Order, OrderService } from '../../../main/order.service';
-import { Customer } from '../../../main/customer.service';
-import { School } from '../../../main/school.service';
-import { Employee } from '../../../main/employee.service';
+import { Customer, CustomerService } from '../../../main/customer.service';
+import { School, SchoolService } from '../../../main/school.service';
+import { Employee, EmployeeService } from '../../../main/employee.service';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { CartDialog } from '../order/cart-dialog/cart-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { UtilService } from '../../../main/util.service';
 import { BepDialog } from '../order/don-hang-cho-bep-dialog/bep-dialog.component';
 
@@ -23,8 +21,16 @@ import { BepDialog } from '../order/don-hang-cho-bep-dialog/bep-dialog.component
 export class OrderListComponent implements OnInit {
   all?: Order[] = [];
   orderFilter?: Order[] = [];
+  customers: Customer[] = [];
+  allSchools: School[] = [];
+  schools: School[] = [];
+  employees: Employee[] = [];
   startDate: any;
   endDate: any;
+  oFilter: any = {};
+  selectKH = '';
+  selectSchool = '';
+  selectEmployee = '';
   settings = {
     add: {
       confirmCreate: true,
@@ -62,6 +68,7 @@ export class OrderListComponent implements OnInit {
           }
           return 0;
         },
+        filter: false,
       },
       customer: {
         title: 'KHÁCH HÀNG',
@@ -74,6 +81,14 @@ export class OrderListComponent implements OnInit {
             return c1.name.localeCompare(c2.name);
           }
           return c2.name.localeCompare(c1.name);
+        },
+        filterFunction(el?: any, search?: string): boolean {
+          const match = el?.name.toUpperCase().indexOf(search.toUpperCase()) > -1;
+          if (match || search === '') {
+            return true;
+          } else {
+            return false;
+          }
         },
       },
       school: {
@@ -88,6 +103,14 @@ export class OrderListComponent implements OnInit {
           }
           return c2.name.localeCompare(c1.name);
         },
+        filterFunction(el?: any, search?: string): boolean {
+          const match = el?.name.toUpperCase().indexOf(search.toUpperCase()) > -1;
+          if (match || search === '') {
+            return true;
+          } else {
+            return false;
+          }
+        },
       },
       employee: {
         title: 'Nhân Viên Giao',
@@ -101,6 +124,14 @@ export class OrderListComponent implements OnInit {
           }
           return c2.name.localeCompare(c1.name);
         },
+        filterFunction(el?: any, search?: string): boolean {
+          const match = el?.name.toUpperCase().indexOf(search.toUpperCase()) > -1;
+          if (match || search === '') {
+            return true;
+          } else {
+            return false;
+          }
+        },
       },
       orderTotal: {
         title: 'Tổng tiền',
@@ -111,6 +142,7 @@ export class OrderListComponent implements OnInit {
           });
           return this.currencyPipe.transform(total, '', '', '1.0-0') + ' VNĐ';
         },
+        filter: false,
       },
     },
     actions: {
@@ -135,6 +167,9 @@ export class OrderListComponent implements OnInit {
     private dialog: MatDialog,
     private utilService: UtilService,
     private datePipe: DatePipe,
+    private customerService: CustomerService,
+    private schoolService: SchoolService,
+    private employeeService: EmployeeService,
   ) {
     if (this.modelService.cacheOrder) {
       this.all = this.modelService.cacheOrder;
@@ -154,10 +189,17 @@ export class OrderListComponent implements OnInit {
         this.source.load(this.orderFilter);
       });
     }
+
+    this.getAllCustomer();
+    this.getAllSchools();
+    this.getAllEmployee();
+
     // Lấy order cho 7 ngày gần nhất
     const date = this.modelService.getLast7Days();
     this.startDate = date[0];
     this.endDate = date[1];
+    this.oFilter.startDate = this.startDate;
+    this.oFilter.endDate = this.endDate;
     this.filterByDate(this.startDate, this.endDate);
   }
 
@@ -202,19 +244,17 @@ export class OrderListComponent implements OnInit {
     if (!(startDate instanceof Date)) {
       this.modelService.filterStartDate = this.utilService.getDateFromString(startDate.value);
       this.modelService.filterEndDate = this.utilService.getDateFromString(endDate.value);
+      this.oFilter.startDate = this.modelService.filterStartDate;
+      this.oFilter.endDate = this.modelService.filterEndDate;
     }
-    this.orderFilter = this.all.filter((o: Order) => {
-      const orderDate = new Date((new Date(o?.date)).setHours(0, 0, 0, 0));
-      return this.modelService.filterStartDate <= orderDate && orderDate <= this.modelService.filterEndDate;
-    });
-    this.source.load(this.orderFilter);
+    this.globalFilter();
   }
 
   indonchobep() {
     let items: Cart[] = [];
-    let date = (new Date()).toLocaleDateString();
+    let date = (new Date()).toLocaleDateString('vi-VN');
     this.orderFilter.forEach((o: Order) => {
-      date = (new Date(o?.date ?? '')).toLocaleDateString();
+      date = (new Date(o?.date ?? '')).toLocaleDateString('vi-VN');
       o.item.forEach((c: Cart) => {
         const exists = items.filter((item: Cart) => item.product.key === c.product.key).length;
         if (exists) {
@@ -240,4 +280,118 @@ export class OrderListComponent implements OnInit {
       this.utilService.gotoPage('pages/chicken/order/' + event.data.key);
     }
   }
+
+  getAllCustomer() {
+    if (this.customerService.cacheCustomers) {
+      this.customers = this.customerService.cacheCustomers;
+    } else {
+      this.customerService.getAll().snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val()}),
+          ),
+        ),
+      ).subscribe(all => {
+        this.customers = this.customerService.cacheCustomers = all;
+      });
+    }
+  }
+
+  getAllEmployee() {
+    if (this.employeeService.cacheEmployees) {
+      this.employees = this.employeeService.cacheEmployees;
+    } else {
+      this.employeeService.getAll().snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val()}),
+          ),
+        ),
+      ).subscribe(all => {
+        this.employees = this.employeeService.cacheEmployees = all;
+      });
+    }
+  }
+
+  getAllSchools() {
+    if (this.schoolService.cacheSchools) {
+      this.schools = this.allSchools = this.schoolService.cacheSchools;
+    } else {
+      this.schoolService.getAll().snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c =>
+            ({key: c.payload.key, ...c.payload.val()}),
+          ),
+        ),
+      ).subscribe(all => {
+        this.schools = this.schoolService.cacheSchools = this.allSchools = all;
+      });
+    }
+  }
+
+  filterByCustomer(c: Customer) {
+    if (c) {
+      this.oFilter.customer = c;
+      this.schools = this.allSchools.filter((s: School) => s.owner === c.key);
+      this.selectSchool = '';
+      this.oFilter.school = null;
+    } else {
+      this.oFilter.customer = null;
+      this.schools = this.allSchools;
+    }
+    this.globalFilter();
+  }
+
+  filterBySchool(c: School) {
+    if (c) {
+      this.oFilter.school = c;
+    } else {
+      this.oFilter.school = null;
+    }
+    this.globalFilter();
+  }
+
+  filterByEmployee(c: Employee) {
+    if (c) {
+      this.oFilter.employee = c;
+    } else {
+      this.oFilter.employee = null;
+    }
+    this.globalFilter();
+  }
+
+  globalFilter() {
+    // Always filter by date 
+    this.orderFilter = this.all.filter((o: Order) => {
+      const orderDate = new Date((new Date(o?.date)).setHours(0, 0, 0, 0));
+      return this.modelService.filterStartDate <= orderDate && orderDate <= this.modelService.filterEndDate;
+    });
+
+    if (this.oFilter.customer) {
+      this.orderFilter = this.orderFilter.filter((o: Order) => o.customer.key === this.oFilter.customer.key);
+    }
+    if (this.oFilter.school) {
+      this.orderFilter = this.orderFilter.filter((o: Order) => o.school.key === this.oFilter.school.key);
+    }
+    if (this.oFilter.employee) {
+      this.orderFilter = this.orderFilter.filter((o: Order) => o.employee.key === this.oFilter.employee.key);
+    }
+    this.source.load(this.orderFilter);
+  }
+
+  resetFilter() {
+    this.modelService.filterStartDate = null;
+    const date = this.modelService.getLast7Days();
+    this.startDate = date[0];
+    this.endDate = date[1];
+    this.oFilter = {
+      startDate: this.startDate,
+      endDate: this.endDate,
+      customer: null,
+      school: null,
+      employee: null,
+    };
+    this.globalFilter();
+  }
+
 }
