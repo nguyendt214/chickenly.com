229195b7fa8e2f046b6529d81f76344e5c464c9f
ndotrong <ndotrong@pentalog.com>
2024-03-29 16:38:48 +0700

    REMOVE GOOGLE MAPS

--- src/app/pages/maps/maps.module.ts
+++ src/app/pages/maps/maps.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { NbCardModule } from '@nebular/theme';
@@ -10,7 +9,6 @@ import { MapsRoutingModule, routedComponents } from './maps-routing.module';
 @NgModule({
   imports: [
     ThemeModule,
-    GoogleMapsModule,
     LeafletModule.forRoot(),
     MapsRoutingModule,
     NgxEchartsModule,
