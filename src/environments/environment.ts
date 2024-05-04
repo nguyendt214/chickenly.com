760f25485d9ed5dec1b274ee72f16921823be56c
ndotrong <ndotrong@pentalog.com>
2024-05-04 15:30:11 +0700

    Test env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,12 +11,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyB4F4fuK6DyVeWfAIQcQfwtmq4tKWsq5D0',
-    authDomain: "chickenly.firebaseapp.com",
-    projectId: "chickenly",
-    storageBucket: "chickenly.appspot.com",
-    messagingSenderId: "690793375468",
-    appId: "1:690793375468:web:affc99d7ea97ebb5ec27c9",
+    apiKey: "AIzaSyD4qdbRj44DR5EF4ouj5gDCuPa08u8zJ84",
+    authDomain: "chickenly-test.firebaseapp.com",
+    projectId: "chickenly-test",
+    storageBucket: "chickenly-test.appspot.com",
+    messagingSenderId: "1088885754446",
+    appId: "1:1088885754446:web:8bc03c52cb1a971967511f",
     measurementId: "G-JDCKD473BF",
   }
 };
